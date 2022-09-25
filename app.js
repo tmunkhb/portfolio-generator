5a0b96cb0f0e08c9cdc6e04efbe7bc01be81f244
tmunkhb <tmunkhb@gmail.com>
2022-09-25 14:57:43 -0400

    generate html file

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
 
-const printProfiledata = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
 
+    console.log('Portfolio complete! Checkout index.html to see the output!');
 
-
-
-
-printProfiledata(profileDataArgs);
+});

644f26462a043ab091c289d44b8b3c18c38740b8
tmunkhb <tmunkhb@gmail.com>
2022-09-25 15:34:13 -0400

    inquirer question

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-templatec');
+// const pageTML = generatePage(name, github);
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw new Error(err);
 
-    console.log('Portfolio complete! Checkout index.html to see the output!');
+//     console.log('Portfolio complete! Checkout index.html to see the output!');
 
-});
+// });
+
+inquirer
+    .prompt([
+        {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
